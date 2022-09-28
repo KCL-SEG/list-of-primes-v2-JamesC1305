a252f4765f39a5f1e4f1d8f9b83b6d1839efbb80
James Curtis <james.curtis@kcl.ac.uk>
2022-09-28 18:07:37 +0100

    Add exceptions to primes

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes <= 0:
+        raise ValueError(f"{number_of_primes} is not valid, value must be greater than 0.")
+    list = [2]
+    n = 3
+    while len(list) < number_of_primes:
+        if n % 2 != 0:
+            f = 3
+            while f * f <= n:
+                if n % f == 0:
+                    break
+                else:
+                    f += 2
+            else:
+                list.append(n)
+        n += 1
     return list
+
+print(primes(-1))
