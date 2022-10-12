4349871bf098e4d1c1ae085a5a03afdee388967b
James <james.curtis@kcl.ac.uk>
2022-10-12 18:03:35 +0100

    Add comments

--- primes.py
+++ primes.py
@@ -4,17 +4,26 @@
 def primes(number_of_primes):
     if number_of_primes <= 0:
         raise ValueError()
+    #2 is the first prime, so is added to the list
     list = [2]
     n = 3
     while len(list) < number_of_primes:
-        if n % 2 != 0:
-            f = 3
-            while f * f <= n:
-                if n % f == 0:
-                    break
-                else:
-                    f += 2
+        #f: every odd number, starting from 3
+        f = 3
+        """The largest a prime factor can be is the root of n,
+        this is because even if a number larger than root n can divide
+        n, there is also a number smaller than root n which is the quotient.
+        So by checking up to root n we know that we have checked all factor pairs"""
+        while f * f <= n:
+            #if the number we are checking is divisible by f then it is not prime
+            if n % f == 0:
+                break
+            #check next odd number
             else:
-                list.append(n)
-        n += 1
+                f += 2
+        #if all odd numbers less than or equal to root n are checked, and none of them divide n, then n is prime
+        else:
+            list.append(n)
+        #increment n to next odd number
+        n += 2
     return list
